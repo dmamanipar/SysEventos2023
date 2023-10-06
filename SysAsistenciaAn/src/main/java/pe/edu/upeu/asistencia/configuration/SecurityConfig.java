d63a42a210388f2d9d4231d24de3ca167c652b3e
davidmp <mamanipari@gmail.com>
2023-10-05 20:57:08 -0500

    prueba de integracion continua 2

--- SysAsistenciaAn/src/main/java/pe/edu/upeu/asistencia/configuration/SecurityConfig.java
+++ SysAsistenciaAn/src/main/java/pe/edu/upeu/asistencia/configuration/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig {
                 .addFilterBefore(new JwtAuthFilter(userAuthenticationProvider), BasicAuthenticationFilter.class)
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(customizer -> customizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests((requests) -> requests
+                .authorizeHttpRequests(req -> req
                         .requestMatchers(HttpMethod.POST, "asis/login",
                                 "asis/register")
                         .permitAll()
